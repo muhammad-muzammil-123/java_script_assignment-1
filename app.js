5d8a50577a0fd86924e54861f307eb1e1e065d8a
Muhammad Muzammil <126906348+muhammad-muzammil-123@users.noreply.github.com>
2023-09-18 09:20:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,212 @@
+// #*#*#*#*#*#*#*#**#*#*#*#*#* CHAPTER (1) #*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+// #######################QUESTION NO:1######################
+// alert("Error!Please enter a valid password");
+
+// ####################### QUESTION NO :2###################
+// alert("Welcome to js land \n happy coding")
+
+// ################# QUESTION NO:3 ##########################
+// alert("welcome to js land.......")
+
+// ######################## QUESTION NO: 4 ###################
+// alert("Happy coding!")
+
+// ##################### QUESTION NO: 5 ##################
+// alert("Hello i can run ja through my web browser control");
+
+
+// #*#*#*#*#*#*#*#*#*#*#*#* CHAPTER NO (2) #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+// ############################QUESTION NO:1#############################
+//  var userName = "muzammil";
+//  alert(userName);
+
+// ############################ QUESTION NO:2########################
+    //  var myName = "muhammad muzammil";
+    //  alert(myName);
+
+// ########################## QUESTION NO:3##################
+//  var message = "hello world";
+//  alert(message);
+
+// ####################### QUESTION NO:4##################
+// var name = "muhammad muzammil";
+// alert(name);
+// var age = 20;
+// alert(age);
+// var study = "web and app development";
+// alert(study);
+
+// ##################### QUESTION NO:5 #################
+//  var fastFood  = "PIZZA";
+//  alert(fastFood);
+//  fastFood = "PIZZ";
+//  alert(fastFood);
+//  fastFood = "PIZ";
+//  alert(fastFood);
+//  fastFood = "PI";
+//  alert(fastFood);
+//  fastFood = "P";
+//  alert(fastFood);
+
+// ############################ QUESTION NO:6 ########################
+//  var email =  "muhammad muzammil@gmail.com";
+// alert("my email adress is " + email);
+
+// ################## QUESTION NO : 7 #####################
+//  var book = "A SMARTER WAY TO LEARN JAVA SCRIPT";
+//  alert("my book name is "+ book);
+
+// ######################### QUESTION NO :8 ################
+// document.write("yah! i can write HTML content in java script")
+
+// #*#*#*#*#*#*#*#*#*#*#*#*#*#*# CHAPTER (3) #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+// ######################## QUESTION NO:1 #############################
+
+//  var age = 20;
+//  alert("I am "+ age + "years old")
+
+//  ##################### QUESTION NO: 2 ###########################
+// var visitors = 14;
+// alert("you have visit in " + 14 + " times");
+
+// ################## QUESTION NO: 3 ################
+// var birthDay = 2003;
+// document.write("my birth day is " + birthDay)
+
+// ################# QUESTION NO:4 ##############
+// var webSiteName = "www.clothing.com";
+// var visitorsName = "muzammil";
+// var productTitle = "T-shirt"; 
+// var productOrder = 5;
+
+// document.write(visitorsName+ " ordered "+ productOrder , productTitle + " in " + webSiteName);
+
+
+// #*#*#*#*#*#*#*#*#*#*#*#*#*#*#**# CHAPTER (4) #*#*#*#*#**#*#**#*#*#*#*#*#*#*
+// ######################QUESTION NO: 1###############3
+//  ################### 5 LEGAL DECLARE VARIABLE ############
+// var name = "muzammil";
+// var myname = "muzammil";
+// var myName = "muzammil";
+// var my$name = "muzammil";
+// var my_name = "muzammil";
+// alert(name);
+// alert(myname);
+// alert(myName);
+// alert(my$name);
+// alert(my_name);
+
+// ################### QUESTION NO: 2############
+// ################# 5 ILLEGAL DECLARE VARIABLE ##############
+//  var Name = "muhammad";
+//  var 1name = "muhammmad";
+//  var my-name = "muhammad";
+//  var name 1 = "muhammad";
+//  var #name = "muhammad";
+
+
+// #*#*#*#*#*#*#*#*#*#*#**# CHAPTER (5) #*#*#*#*#*#**#*#*#*#*#*
+// // #########################QUESTION NO :1#############
+// var num1 = 5;
+// var num2 = 3;
+// var num3 = num1 + num2;
+// document.write("the sum of num1 and num2 is " + num3);
+
+// ################ QUESTION NO :2 ###############
+//  var num1 = 
+//  document.write("value after decleration is " + num1 + "<br>");
+//  var initialNum = 5;
+//  document.write("Initial value is " + initialNum + "<br>" );
+// var increamentNum = initialNum + 1;
+// document.write("the value after increament is " + increamentNum + "<br>");
+// var addition =   increamentNum  + 7;
+// document.write("the value after addition is " + addition + "<br>")
+// var decreamentNum = addition - 1;
+// document.write("the value affter decrement is " + decreamentNum + "<br>")
+// var remainder = decreamentNum % 3;
+// document.write("the remainder is " + remainder);
+
+// ##################### QUESTION NO:3 #################
+//  var ticketPrice = 600;
+//  var fiveTickets = 5;
+//  var totalPrice = 5 *600;
+//  document.write("the total tickets price is " + totalPrice)
+
+// ################### QUESTION NO:4 ##################
+
+// var table = "TABLE OF 4";
+// var table1 = 4*1;
+// var table2 = 4*2
+// var table3 = 4*3
+// var table4 = 4*4
+// var table5 = 4*5
+// var table6 = 4*6
+// var table7 = 4*7
+// var table8 = 4*8
+// var table9 = 4*9
+// var table10 = 4*10
+// document.write(table  +" <br>" );
+// document.write("4 X 1 = " + table1 +  " <br>");
+// document.write("4 X 2 = " + table2 +  " <br>");
+// document.write("4 X 3 = " + table3 +  " <br>");
+// document.write("4 X 4 = " + table4 +  " <br>");
+// document.write("4 X 5 = " + table5 +  " <br>");
+// document.write("4 X 6 = " + table6 +  " <br>");
+// document.write("4 X 7 = " + table7 +  " <br>");
+// document.write("4 X 8 = " + table8 +  " <br>");
+// document.write("4 X 9 = " + table9 +  " <br>");
+// document.write("4 X 10 = " + table10 +  " <br>");
+
+// ############### QUESTION NO: 5###################
+// var priceOfItem1 = 650;
+// var quantityOfItem1 = 3;
+// var priceOfItem2 = 700;
+// var quantityOfItem2 = 4;
+// var shippingCharges = 100;
+// var totalPrice = (priceOfItem1*quantityOfItem1) + (priceOfItem2*quantityOfItem2) + shippingCharges
+// document.write("total price is " + totalPrice);
+
+// #################### QUESTION NO:6 ######################
+// document.write("<h1>MARKS SHEET</h1>")
+// var totalMarks = 980;
+// var obtainedMarks = 809;
+// document.write("total marks is : " + totalMarks + "<br>");
+// document.write("obtained  marks is : " + obtainedMarks+ "<br>");
+// document.write("percentage is " + obtainedMarks / totalMarks *100 )
+
+// ####################### QUESTION NO:7 #################
+//  var dollar =  104 *10;
+//  var saudiCurrency = 25 *28;
+//  var total = dollar + saudiCurrency
+//  document.write("the total currency of PKRS ", total )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
